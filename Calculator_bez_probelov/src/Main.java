0439ad822ab1a84476555699db7729e0e23b8dd4
axaviii <99210520+axaviii@users.noreply.github.com>
2023-09-29 23:35:28 +0400

    Add files via upload
    Калькулятор вторая версия. Теперь вводить с клавиатуры можно без пробелов. Пока не придумал как делать римские цифры
    
    Создай консольное приложение "Калькулятор". Приложение должно читать из консоли введенные пользователем строки, числа, арифметические операции проводимые между ними и выводить в консоль результат их выполнения.
    Реализуй класс Main с методом public static String calc(String input). Метод должен принимать строку с арифметическим выражением между двумя числами и возвращать строку с результатом их выполнения. Ты можешь добавлять свои импорты, классы и методы. Добавленные классы не должны иметь модификаторы доступа (public или другие).
    Требования:﻿
    Калькулятор умеет выполнять операции сложения, вычитания, умножения и деления с двумя числами: a + b, a - b, a * b, a / b. Данные передаются в одну строку (смотри пример)! Решения, в которых каждое число и арифмитеческая операция передаются с новой строки считаются неверными.
    Калькулятор умеет работать как с арабскими (1,2,3,4,5...), так и с римскими (I,II,III,IV,V...) числами.
    Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более. На выходе числа не ограничиваются по величине и могут быть любыми.
    Калькулятор умеет работать только с целыми числами.
    Калькулятор умеет работать только с арабскими или римскими цифрами одновременно, при вводе пользователем строки вроде 3 + II калькулятор должен выбросить исключение и прекратить свою работу.
    При вводе римских чисел, ответ должен быть выведен римскими цифрами, соответственно, при вводе арабских - ответ ожидается арабскими.
    При вводе пользователем неподходящих чисел приложение выбрасывает исключение и завершает свою работу.
    При вводе пользователем строки, не соответствующей одной из вышеописанных арифметических операций, приложение выбрасывает исключение и завершает свою работу.
    Результатом операции деления является целое число, остаток отбрасывается.
    Результатом работы калькулятора с арабскими числами могут быть отрицательные числа и ноль. Результатом работы калькулятора с римскими числами могут быть только положительные числа, если результат работы меньше единицы, выбрасывается исключение

---
+++ Calculator_bez_probelov/src/Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+try {
+
+
+    String input = sc.nextLine();
+    String result = calculate(input);
+    System.out.println("Ответ: " + result);
+}catch (IllegalArgumentException e){
+    System.out.println("Ошибка " + e.getMessage());
+}finally {
+    sc.close();
+      }
+    }
+
+    static String calculate(String input) {
+        char operator = findOperator(input);
+        String[] operands = input.split("\\" + operator);
+        if (operands.length !=2){
+            throw new IllegalArgumentException("Неверный формат выражения.");
+        }
+        int operand1 = Integer.parseInt(operands[0]);
+        int operand2 = Integer.parseInt(operands[1]);
+
+        if ((operand1 < 1 || operand1 > 10) || (operand2 < 1 || operand2 > 10)) {
+            throw new IllegalArgumentException("Числа должны быть в диапазоне от 1 до 10 включительно.");
+        }
+        int result;
+
+        switch (operator){
+            case '+' : result = operand1 + operand2;
+            break;
+            case '-' : result = operand1 - operand2;
+                break;
+            case '/' :
+                if(operand2 ==0){throw new IllegalArgumentException("на ноль делить нельзя");}
+                    result = operand1 / operand2;
+                break;
+            case '*' : result = operand1*operand2;
+                break;
+            default: throw new IllegalArgumentException("Пока калькулятор умеет делать только эти операции");
+        }
+        return String.valueOf(result);
+    }
+
+     static char findOperator(String input) // поиск первого встечного оператора " + - / *
+    {
+        for(char operator : "+-/*".toCharArray()) // с помощью toCharArray  мы перевоим строку в массив чаров
+            {
+            if (input.contains(String.valueOf(operator))){
+                return operator;
+            }
+
+        }
+        throw new IllegalArgumentException("Оператор не найден");
+    }
+}
