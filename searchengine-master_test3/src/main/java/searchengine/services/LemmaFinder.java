1aa10c25d797a66cea3f96f74f978cc71162332c
andrejalberdin <alberdin5@mail.ru>
2025-01-02 13:59:22 +0400

    13:59

--- searchengine-master_test3/src/main/java/searchengine/services/LemmaFinder.java
+++ searchengine-master_test3/src/main/java/searchengine/services/LemmaFinder.java
@@ -1,6 +1,7 @@
 package searchengine.services;
 
 import org.apache.lucene.morphology.LuceneMorphology;
+import org.apache.lucene.morphology.english.EnglishLuceneMorphology;
 import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
 import org.jsoup.Jsoup;
 
@@ -9,39 +10,62 @@ import java.util.*;
 
 
 public class LemmaFinder {
-    private final LuceneMorphology luceneMorphology;
+    private final LuceneMorphology russianMorphology;
+    private final LuceneMorphology englishMorphology;
+    private static final String RUSSIAN_WORLD_REGEX = "[а-яА-ЯёЁ]+$";
+    private static final String ENGLISH_WORLD_REGEX = "[a-zA-Z]+$";
     private static final String WORD_TYPE_REGEX = "\\W\\w&&[^а-яА-Я\\s]";
     private static final String[] particlesNames = new String[] {"МЕЖД", "ПРЕДЛ", "СОЮЗ"};
 
-    public LemmaFinder(LuceneMorphology luceneMorphology) {
-        this.luceneMorphology = luceneMorphology;
+    public LemmaFinder(LuceneMorphology russianMorphology, LuceneMorphology englishMorphology) {
+        this.russianMorphology = russianMorphology;
+        this.englishMorphology = englishMorphology;
     }
 
     public static LemmaFinder getInstance() throws IOException {
-        LuceneMorphology morphology = new RussianLuceneMorphology();
-        return new LemmaFinder(morphology);
+        LuceneMorphology russian = new RussianLuceneMorphology();
+        LuceneMorphology english= new EnglishLuceneMorphology();
+        return new LemmaFinder(russian, english);
     }
 
     private LemmaFinder() {
         throw new RuntimeException("Disallow construct");
     }
+
+
+    //Определяем какой анализатор использовать
+    private LuceneMorphology getMorphology(String word) {
+        if(word.matches(RUSSIAN_WORLD_REGEX)) {
+            return russianMorphology;
+        }else if(word.matches(ENGLISH_WORLD_REGEX)) {
+            return englishMorphology;
+        }
+        return null;  // для случаев чисел или символов
+    }
+
+
     /**
      * Метод разделяет текст на слова, находит все леммы и считает их количество
      * @return ключ является леммой, а значение количеством найденных лемм
      * @Param text текст из которого будут выбираться леммы
      */
     public Map<String, Integer> collectLemmas(String text) {
-        String[] words = arrayContainsRussianWords(text);
+        String[] words = arrayContainsRussianWordsAndEnglish(text);
         HashMap<String, Integer> lemmas = new HashMap<>();
         for (String word : words) {
             if (word.isBlank()) {
                 continue;
             }
-            List<String> wordBaseForms = luceneMorphology.getMorphInfo(word);
+            LuceneMorphology morphology = getMorphology(word);
+            if (morphology != null) {
+                continue;
+            }
+
+            List<String> wordBaseForms = morphology.getMorphInfo(word);
             if (anyWordBaseBelongToParticle(wordBaseForms)) {
                 continue;
             }
-            List<String> normalForms = luceneMorphology.getNormalForms(word);
+            List<String> normalForms = morphology.getNormalForms(word);
             if (normalForms.isEmpty()) {
                 continue;
             }
@@ -59,15 +83,19 @@ public class LemmaFinder {
      * @return набор уникальных лемм найденных в тексте
      */
     public Set<String> getLemmaSet(String text){
-        String[] textArray = arrayContainsRussianWords(text);
+        String[] textArray = arrayContainsRussianWordsAndEnglish(text);
         Set<String> lemmaSet = new HashSet<>();
         for(String word : textArray){
-            if(!word.isEmpty() && isCorrectWordForm(word)){
-                List<String> wordBaseForms = luceneMorphology.getMorphInfo(word);
+            if(!word.isEmpty() /*&& isCorrectWordForm(word)*/){
+                LuceneMorphology morphology = getMorphology(word);
+                if (morphology != null) {
+                    continue;
+                }
+                List<String> wordBaseForms = morphology.getMorphInfo(word);
                 if(anyWordBaseBelongToParticle(wordBaseForms)){
                     continue;
                 }
-                lemmaSet.addAll(luceneMorphology.getNormalForms(word));
+                lemmaSet.addAll(morphology.getNormalForms(word));
             }
         }
         return lemmaSet;
@@ -87,13 +115,17 @@ public class LemmaFinder {
        return false;
     }
 
-    private String[] arrayContainsRussianWords(String text){
+    private String[] arrayContainsRussianWordsAndEnglish(String text){
         return text.toLowerCase(Locale.ROOT)
-                .replaceAll("([^а-я\\s])", " ")
+                .replaceAll("([^а-яёa-z\\s])", " ")
                 .trim()
                 .split("\\s+");
     }
-    private boolean isCorrectWordForm(String word){
+
+
+
+
+   /* private boolean isCorrectWordForm(String word){
         List<String> wordInfo = luceneMorphology.getMorphInfo(word);
         for (String morphInfo : wordInfo){
             if(morphInfo.contains(WORD_TYPE_REGEX)){
@@ -101,7 +133,7 @@ public class LemmaFinder {
             }
         }
         return true;
-    }
+    }*/
 
     /**
      * Метод очищает HTML-код и возвращает только текст.
