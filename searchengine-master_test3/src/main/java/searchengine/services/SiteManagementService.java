099bf0b2dcdb7af74d2b9f82c96de3a506959719
andrejalberdin <alberdin5@mail.ru>
2025-01-07 17:19:56 +0400

    17:19

--- searchengine-master_test3/src/main/java/searchengine/services/SiteManagementService.java
+++ searchengine-master_test3/src/main/java/searchengine/services/SiteManagementService.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import searchengine.model.SiteEntity;
+import searchengine.repository.IndexRepository;
 import searchengine.repository.PageRepository;
 import searchengine.repository.SiteRepository;
 
@@ -13,14 +14,17 @@ public class SiteManagementService {
     private final SiteRepository siteRepository;
     private final PageRepository pageRepository;
     private static final Logger logger = LoggerFactory.getLogger(IndexingService.class);
+    private final IndexRepository indexRepository;
 
-    public SiteManagementService(SiteRepository siteRepository, PageRepository pageRepository) {
+    public SiteManagementService(SiteRepository siteRepository, PageRepository pageRepository, IndexRepository indexRepository) {
         this.siteRepository = siteRepository;
         this.pageRepository = pageRepository;
+        this.indexRepository = indexRepository;
     }
 
     @Transactional
     public void deleteSiteData(SiteEntity siteEntity) {
+        indexRepository.deleteAll();
         pageRepository.deleteBySiteEntity(siteEntity);
         siteRepository.deleteById(siteEntity.getId());
         logger.info("Удаление существующего сайта: {}", siteEntity.getUrl());
